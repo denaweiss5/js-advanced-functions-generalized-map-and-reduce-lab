e80047407375b0e8def261a46b9cbd5c607a70bb
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-22 15:10:04 -0600

    commit tree bf1ff96ad0a6424e62d8db3d7255260cf58849d6

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(src, cb){
+let r = []
+
+for (let i=0; i<src.length; i++){
+    let theEl = src[i]
+    r.push(cb(theEl))
+}
+return r 
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
